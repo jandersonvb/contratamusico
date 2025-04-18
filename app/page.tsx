44a466e1ec9f3f7b0c088948f48c25edad037668
Janderson Vilas Boas <jandersonvb.dev@gmail.com>
2025-04-18 01:26:43 -0300

    feat: create first page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Hello, Next.js!</h1>
-      <p className="mt-4 text-lg">Welcome to your Next.js application.</p>
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-6">
+      <h1 className="text-3xl font-bold">ContrataMúsico 🚀</h1>
+      <p className="text-lg">A plataforma que conecta músicos e contratantes.</p>
+      <p className="text-lg">Em breve, mais informações!</p>
+
+
+      <Button asChild>
+        <a
+          href="https://wa.me/5535998102070"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white bg-green-500 hover:bg-green-600"
+        >
+          Fale comigo no WhatsApp
+        </a>
+      </Button>
+    </main >
   );
 }
